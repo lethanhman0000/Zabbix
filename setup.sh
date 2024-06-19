584c6a1cf6df3fa06af0e210ee2d303cc8a399e3
Yakuno Sumei <128463517+lethanhman0000@users.noreply.github.com>
2024-06-19 16:38:43 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,18 +25,20 @@ DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 EOF
-# Create Zabbix database and user
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "create user 'zabbix'@'localhost' identified by '123456';"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "grant all privileges on zabbix.* to 'zabbix'@'localhost';"
-mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "set global log_bin_trust_function_creators = 1;"
 
 # Install Zabbix repository
 wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-1+ubuntu24.04_all.deb
 dpkg -i zabbix-release_7.0-1+ubuntu24.04_all.deb
 
 # Install Zabbix server, frontend, agent
-apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent -y
+apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent mysql-servver -y
+systemctl start mysql
+
+# Create Zabbix database and user
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "create user 'zabbix'@'localhost' identified by '123456';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "grant all privileges on zabbix.* to 'zabbix'@'localhost';"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "set global log_bin_trust_function_creators = 1;"
 
 # Import initial schema and data for Zabbix server
 zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix
