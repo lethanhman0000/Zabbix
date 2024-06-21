b4d43e40b4f6d0c0ab0d64bf96b9a12426030e95
Yakuno Sumei <128463517+lethanhman0000@users.noreply.github.com>
2024-06-21 11:13:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,19 +11,19 @@ sudo apt-get -y upgrade
 sudo apt-get install -y wget gnupg2 lsb-release
 
 # Download Zabbix repository package
-wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+ubuntu20.04_all.deb
+wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-1+ubuntu24.04_all.deb
 
 # Install the Zabbix repository package
-sudo apt-get install -y ./zabbix-release_5.0-1+ubuntu20.04_all.deb
+sudo apt-get install ./zabbix-release_7.0-1+ubuntu24.04_all.deb
 
 # Update package index
 sudo apt-get update
 
 # Install Zabbix server, frontend, and agent
-sudo apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
+sudo apt-get install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
 
 # Install MariaDB
-sudo apt-get install -y mariadb-server
+sudo apt-get install mariadb-server
 
 # Start and enable MariaDB service
 sudo systemctl start mariadb
@@ -42,10 +42,10 @@ EXIT;
 EOF
 
 # Import initial schema and data for Zabbix server
-zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -p'your_password' zabbix
+zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -p'123456' zabbix
 
 # Update Zabbix server configuration file
-sudo sed -i 's/# DBPassword=/DBPassword=your_password/' /etc/zabbix/zabbix_server.conf
+sudo sed -i 's/# DBPassword=/DBPassword=123456/' /etc/zabbix/zabbix_server.conf
 
 # Set timezone in PHP configuration for Zabbix
 echo "php_value[date.timezone] = Asia/Ho_Chi_Minh" | sudo tee /etc/php/7.4/apache2/conf.d/99-zabbix.ini
